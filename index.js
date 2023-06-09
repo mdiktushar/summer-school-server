a6e3c18d560c49987f0d4d4216b10850c86a2144
mdiktushar <md.tushar1804@gmail.com>
2023-06-09 16:21:20 +0600

    jwt install

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
