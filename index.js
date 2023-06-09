7b8b19baaf2058b8b9ef4decaf313de459d90448
mdiktushar <md.tushar1804@gmail.com>
2023-06-10 01:33:06 +0600

    user role patch

--- index.js
+++ index.js
@@ -104,6 +104,21 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/:role/:id", async (req, res) => {
+      const id = req.params.id;
+      const newRole = req.params.role;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: newRole,
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
